0c89e2ab8aa10e5d4891d19bada6feb8160c8c1b
Sofiia Novychenko <milakotiy@gmail.com>
2025-10-05 22:49:07 +0200

    add styled loader

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,5 +1,11 @@
+import { PulseLoader } from 'react-spinners';
+
 function Loader() {
-  return <div>Loader</div>;
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-bg-main/50 z-50">
+      <PulseLoader color="#3470ff" size={25} />
+    </div>
+  );
 }
 
 export default Loader;
